1bf9ecd9b1b508b802cb30153ce7ec8522f1297f
thejayadad <74875595+thejayadad@users.noreply.github.com>
2024-05-15 09:54:11 -0400

    second update

--- app/(website)/dashboard/_components/projectpage/update-project-items.js
+++ app/(website)/dashboard/_components/projectpage/update-project-items.js
@@ -1,7 +1,7 @@
 'use client'
 // UpdateProjectItem.js
 import { Input, Textarea, Button } from '@nextui-org/react';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
@@ -25,17 +25,26 @@ const UpdateProjectItem = ({ project }) => {
   });
 
   const [selectedColor, setSelectedColor] = useState(project.color); // State for selected color
+  const [selectedPriority, setSelectedPriority] = useState(project.priority); // State for selected priority
   const [updating, setUpdating] = useState(false); // State for updating status
   const router = useRouter();
+
+  useEffect(() => {
+    setSelectedPriority(project.priority);
+  }, [project.priority]);
 
   const onSelectColor = (color) => {
     setSelectedColor(color); // Update selected color
   };
 
+  const handlePriority = (priority) => {
+    setSelectedPriority(priority); // Update selected priority
+  };
+
   const onSubmit = async (data) => {
     try {
       setUpdating(true); // Set updating status to true
-      await updateProject(project.id, { ...data, color: selectedColor }); // Pass selected color to updateProject function
+      await updateProject(project.id, { ...data, priority: selectedPriority, color: selectedColor }); // Pass priority and selected color to updateProject function
       toast.success('Project has been updated');
       router.push('/dashboard'); // Redirect to dashboard after successful update
     } catch (error) {
@@ -53,10 +62,32 @@ const UpdateProjectItem = ({ project }) => {
         <form className='flex flex-col gap-6' onSubmit={handleSubmit(onSubmit)}>
           <Textarea control={control} name="notes" defaultValue={project.notes} error={errors.notes?.message} />
           <ColorPicker defaultValue={selectedColor} onSelectColor={onSelectColor} /> {/* Pass defaultValue and onSelectColor */}
+          <div className='pt-8 pb-8'>
+            <span className='mb-4'>Select Project Priority</span>
+            <div className="flex space-x-4">
+              <Button 
+                className={`w-full ${selectedPriority === 'low' ? 'bg-primary text-white' : 'bg-light'} ${updating ? 'disabled' : ''}`}
+                onClick={() => handlePriority('low')} disabled={updating}>
+                Low
+              </Button>
+              <Button 
+                className={`w-full ${selectedPriority === 'medium' ? 'bg-success text-white' : 'bg-light'} ${updating ? 'disabled' : ''}`}
+                onClick={() => handlePriority('medium')} disabled={updating}>
+                Medium
+              </Button>
+              <Button 
+                className={`w-full ${selectedPriority === 'high' ? 'bg-warning text-white' : 'bg-light'} ${updating ? 'disabled' : ''}`}
+                onClick={() => handlePriority('high')} disabled={updating}>
+                High
+              </Button>
+            </div>
+
+          </div>
+          
           <Button
-          color='primary'
-          type="submit" auto disabled={updating}> {/* Disable button when updating */}
-            {updating ? 'Updating...' : 'Update'} {/* Show "Updating..." or "Update" based on updating status */}
+            color='primary'
+            type="submit" auto disabled={updating}>
+            {updating ? 'Updating...' : 'Update'}
           </Button>
         </form>
         <Toaster />
